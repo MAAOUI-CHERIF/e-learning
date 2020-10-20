247a8f843d7f2afee044dad2ebe9ea966869d228
Gilliot Mathieu <mathieu.gilliot.pro@gmail.com>
2020-10-20 10:11:33 +0200

    Premier Jet Back + connexion bdd

--- Back/Data/Data.js
+++ Back/Data/Data.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+const UserModel = require('../Sources/Models/UsersModels')
+
+ mongoose.connect('mongodb+srv://Mathieu:Su04za08ku90@e-learning.c1izm.mongodb.net/e-learning?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
 
 
-
+mongoose.connection.once('open',()=>{
+    console.log('Toi, tu rentres')
+})
+.on('error',(error)=>{
+    console.log('Oups...'+error)
+})
 
 
 
--- Back/app.js
+++ Back/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes')
+const routes = require('./routes/Users_routes')
+const bodyParser = require('body-parser')
 
 
+app.use(bodyParser.json())
 app.use(express.static('../Client'));
 
-app.use('/test', routes)
+app.use('/', routes)
 
 module.exports = app;
